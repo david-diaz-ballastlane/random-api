e563a8fb83bd3b0f120e8aa637c298ccf583e247
ddiazghub <dedemoya@uninorte.edu.co>
2025-03-15 17:24:03 -0500

    feature: add rng endpoint

--- index.js
+++ index.js
@@ -6,4 +6,9 @@ app.get('/', (req, res) => {
   res.status(200).send('Hello World!!!')
 })
 
+app.get('/rng', (req, res) => {
+  const rng = Math.random() * Number.MAX_SAFE_INTEGER
+  res.status(200).send(String(rng))
+})
+
 app.listen(3000, () => console.log('Server running in port 3000'))
