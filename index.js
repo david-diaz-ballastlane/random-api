10ff476813949feebf7879e745642f4a3f81ce68
ddiazghub <dedemoya@uninorte.edu.co>
2025-03-15 17:59:37 -0500

    feature: add automatic deployment

--- index.js
+++ index.js
@@ -11,9 +11,6 @@ app.get('/rng', (req, res) => {
   res.status(200).send(String(rng))
 })
 
-// Comment
-// Comment
-// Comment
 app.get('/rng/int', (req, res) => {
   const rng = Math.round(Math.random() * Number.MAX_SAFE_INTEGER)
   res.status(200).send(String(rng))
